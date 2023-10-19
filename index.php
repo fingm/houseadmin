46a6cbe1bc48a9bf9c5b8652381c4ffe24f62ba9
fingm <matiascartro@gmail.com>
2023-10-19 20:42:08 -0300

    comienzo

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    
+<h3>asd</h3>
+<div class="container">
+    <div class="col">
+        <div class="row">
+            asdasd
+        </div>
+    </div>
+</div>
+<form method="POST" action="index.php">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
+
+    <script src="./bpptstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
